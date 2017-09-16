87093bdd6695a381438695299b5b098af5892f88
AGodinez <ing.a.godinez@gmail.com>
2017-09-16 16:31:32 -0500

    proyecto finalizado

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ new Vue({
   el: '#AppVue',
   data:{
     lists:[
-      {keep:'uno', completed:false},
-      {keep:'dos', completed:false},
-      {keep:'tres', completed:true},
+
     ],
+    newKeep: ''
+  },
+  methods: {
+    addKeep: function(){
+      this.lists.push({keep: this.newKeep, completed:false});
+      this.newKeep = '';
+    }
   }
+
 
 });
