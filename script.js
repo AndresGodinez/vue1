e46af016056cc6cda9788fd7ed19f7343c41850a
Andres Vagrant <ing.a.godinez@gmail.com>
2017-09-16 18:35:33 +0000

    primer commit de vue js
    lista dinamica, se crean elementos de una lista
    al estar contenidos dentro de un array en el vue
    para iniciar la clase se hace un new vue
    luego se declara el elemento a utilizar ya sea con id, clase
    despues en el data: se declara el array con sus respectivas llaves de acceso
    dentro del html se hace un for con v-for="item in lists" y se imprimen entre llaves

---
+++ script.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#AppVue',
+  data:{
+    lists:[
+      {keep:'uno', completed:false},
+      {keep:'dos', completed:false},
+      {keep:'tres', completed:true},
+    ],
+  }
+
+});
